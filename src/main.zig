ba6177451ac1b39aeff0ebcb12430eb86c96b7d0
RedPhoenixQ <steve.dark.03@gmail.com>
2025-03-11 21:04:58 +0100

    Add first Layout test and output to csv files

--- src/main.zig
+++ src/main.zig
@@ -4,32 +4,54 @@ const native_endian = @import("builtin").cpu.arch.endian();
 const measure = @import("measure.zig");
 const ArgTypes = @import("common.zig").ArgTypes;
 
+const Layout = @import("Layout.zig");
+
 pub fn main() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer().any();
+    var arena_instance = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const arena = arena_instance.allocator();
 
-    const TestMeasure = struct {
-        rand: std.Random.Pcg,
-        fn init() @This() {
-            return .{ .rand = std.Random.Pcg.init(123) };
+    const cwd = std.fs.cwd();
+    const now = std.time.timestamp();
+
+    var buf: [2048]u8 = undefined;
+
+    const iterations = 30;
+    var seed: u64 = @intCast(100);
+    var size: usize = 1 << 3;
+    while (size < 1024 * 1024 * 2) : (size <<= 1) {
+        seed += 1;
+        const dir_name = try std.fmt.bufPrint(&buf, "./output/layout/{d}/{d}", .{ now, size });
+        var dir = try cwd.makeOpenPath(dir_name, .{});
+        defer dir.close();
+        std.debug.print("Seed: {d}, size: {d}, Memory: {d}\n", .{ seed, size, std.fmt.fmtIntSizeBin(@sizeOf(Layout.AccessAllFields) * size) });
+
+        {
+            defer std.debug.assert(arena_instance.reset(.retain_capacity));
+            const samples, _ = try measure.run(
+                arena,
+                iterations,
+                Layout.AccessAllFields.run_aos,
+                Layout.AccessAllFields.setup_aos,
+                @constCast(&Layout.Gen.init(seed, size)),
+            );
+            const file = try dir.createFile("access_all_fields_aos.csv", .{ .mode = 0o666 });
+            defer file.close();
+            try measure.writeToCSV(samples, file.writer().any());
         }
-        fn run(int: u32) u32 {
-            std.debug.print("hahahahah {d}\n", .{int});
-            return int;
+        {
+            defer std.debug.assert(arena_instance.reset(.retain_capacity));
+            const samples, _ = try measure.run(
+                arena,
+                iterations,
+                Layout.AccessAllFields.run_soa,
+                Layout.AccessAllFields.setup_soa,
+                @constCast(&Layout.Gen.init(seed, size)),
+            );
+            const file = try dir.createFile("access_all_fields_soa.csv", .{ .mode = 0o666 });
+            defer file.close();
+            try measure.writeToCSV(samples, file.writer().any());
         }
-        fn next(self: *@This(), alloc: std.mem.Allocator) ArgTypes(run) {
-            _ = alloc;
-            return .{self.rand.random().int(u32)};
-        }
-    };
-
-    const samples, const returns = try measure.run(std.heap.page_allocator, 10, TestMeasure.run, TestMeasure.next, @constCast(&TestMeasure.init()));
-
-    std.debug.print("Hash: {any}\n", .{returns});
-
-    try measure.writeToCSV(samples, stdout);
-    try bw.flush();
+    }
 }
 
 test {
