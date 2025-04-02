3bfa746c256e34066ac8729b5dbc386cce8ecc94
RedPhoenixQ <steve.dark.03@gmail.com>
2025-04-02 23:53:47 +0200

    More iterations of plot code

--- plot.py
+++ plot.py
@@ -40,7 +40,7 @@ for name in filenames:
 df: pl.DataFrame = pl.concat(frames)
 df = df.with_columns(
     (pl.col("instructions") / pl.col("cpu_cycles"))
-        .alias("inst_per_cycle"),
+        .alias("instructions_per_cycle"),
     pl.when(pl.col("cache_references") > 0)
         .then((pl.col("cache_misses") / pl.col("cache_references")))
         .otherwise(0)
@@ -51,11 +51,33 @@ df = df.with_columns(
         .alias("branch_miss_rate"),
 )
 
+# points = alt.Chart(df.filter(part="ControlFlow")).mark_point().encode(
+#     alt.X("wall_clock", scale=alt.Scale(type="log")),
+#     alt.Y("branch_miss_rate"),
+#     color="test",
+# )
+# regressions = [points.transform_regression("instructions_per_cycle", "branch_miss_rate", groupby=["test"], method="poly", order=4)
+#     .mark_line()
+#     .encode(color="test")
+# ]
+# alt.layer(points,).save(fig_dir.joinpath("branch_miss_rate-scatter.png"), scale_factor=4)
+
+# points = alt.Chart(df.filter(part="ControlFlow")).mark_point().encode(
+#     alt.X("wall_clock", scale=alt.Scale(type="log")),
+#     alt.Y("cache_miss_rate"),
+#     color="test",
+# )
+# regressions = [points.transform_regression("instructions_per_cycle", "cache_miss_rate", groupby=["test"], method="linear")
+#     .mark_line()
+#     .encode(color="test")
+# ]
+# alt.layer(points, ).save(fig_dir.joinpath("cache_miss_rate-scatter.png"), scale_factor=4)
+
 summary = df.group_by("size", "part", "scenario", "kind").agg(
     pl.mean("wall_clock"),
     pl.std("wall_clock").name.suffix("_std"),
-    pl.mean("inst_per_cycle"),
-    pl.std("inst_per_cycle").name.suffix("_std"),
+    pl.mean("instructions_per_cycle"),
+    pl.std("instructions_per_cycle").name.suffix("_std"),
     pl.mean("cache_miss_rate"),
     pl.std("cache_miss_rate").name.suffix("_std"),
     pl.mean("branch_miss_rate"),
@@ -64,22 +86,22 @@ summary = df.group_by("size", "part", "scenario", "kind").agg(
 
 summary.write_csv(fig_dir.joinpath("summary.csv"))
 
-def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined):
+def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined, chart_layers=None):
     error_bars = alt.Chart(source).mark_errorbar(extent=extent, ticks=True).encode(
-        alt.X("size").type("ordinal").title("Size"),
+        alt.X("size", type="ordinal", title="Size"),
         alt.Y(col,
             aggregate=agg,
-            scale=alt.Scale(type=scale), 
+            scale=alt.Scale(type=scale, base=2), 
             axis=alt.Axis(format=y_format),
             title=col.replace("_", " ").capitalize(),
         ),
         xOffset="kind",
     )
     line = alt.Chart(source).mark_bar().encode(
-        alt.X("size").type("ordinal").title("Size"),
+        alt.X("size", type="ordinal", title="Size"),
         alt.Y(col, 
             aggregate=agg,
-            scale=alt.Scale(type=scale), 
+            scale=alt.Scale(type=scale, base=2), 
             axis=alt.Axis(format=y_format),
             title=col.replace("_", " ").capitalize(),
         ),
@@ -90,20 +112,20 @@ def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="
 
 def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined, color="kind", save=True, chart_layers=[]):
     error_bars = alt.Chart(source).mark_errorband(extent=extent).encode(
-        alt.X("size").type("ordinal").title("Size"),
+        alt.X("size", type="quantitative", title="Size", scale=alt.Scale(type="log", base=2, paddingOuter=0)),
         alt.Y(col,
             aggregate=agg,
-            scale=alt.Scale(type=scale), 
+            scale=alt.Scale(type=scale, base=2), 
             axis=alt.Axis(format=y_format),
             title=col.replace("_", " ").capitalize(),
         ),
         color=color,
     )
     line = alt.Chart(source).mark_line().encode(
-        alt.X("size").type("ordinal").title("Size"),
+        alt.X("size", type="quantitative", title="Size", scale=alt.Scale(type="log", base=2)),
         alt.Y(col,
             aggregate=agg,
-            scale=alt.Scale(type=scale), 
+            scale=alt.Scale(type=scale, base=2), 
             axis=alt.Axis(format=y_format), 
             title=col.replace("_", " ").capitalize(),
         ),
@@ -116,14 +138,16 @@ def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent=
 
 
 
-cache_l1 = alt.Chart(pl.from_dict({"x": (64 * 1024 ) // 24})).mark_rule(strokeDash=[12, 6], size=2).encode(x=alt.X("x", type="ordinal"))
-cache_l2 = alt.Chart(pl.from_dict({"x": (256 * 1024 ) // 24})).mark_rule(strokeDash=[12, 6], size=2).encode(x=alt.X("x", type="ordinal"))
-cache_l3 = alt.Chart(pl.from_dict({"x": (9 * 1024 * 1024 ) // 24})).mark_rule(strokeDash=[12, 6], size=2).encode(x=alt.X("x", type="ordinal"))
 cache_lines = [
-    cache_l1, cache_l1.mark_text(text="L1", y=15, dx=-5, align="right", baseline="bottom"),
-    cache_l2, cache_l2.mark_text(text="L2", y=15, dx=-5, align="right", baseline="bottom"),
-    cache_l3, cache_l3.mark_text(text="L3", y=15, dx=-5, align="right", baseline="bottom"),
+    alt.Chart(pl.from_dict({"x": cache_size // 24}))
+        .mark_rule(strokeDash=[12, 6], size=2)
+        .encode(alt.X("x", type="quantitative", scale=alt.Scale(type="log", base=2)))
+    for cache_size in [(64 * 1024), (256 * 1024), (9 * 1024 * 1024)]
 ]
+cache_lines.extend([
+    line.mark_text(text="L" + str(i), y=15, dx=-5, align="right", baseline="bottom")
+    for i, line in enumerate(cache_lines)
+])
 
 
 
@@ -135,9 +159,9 @@ alt.layer(*[
 ]).save(fig_dir.joinpath("ControlFlow-wall_clock-ci.png"), scale_factor=4)
 
 alt.layer(*[
-    per_size_line("", data, "inst_per_cycle", extent="ci", color="test", save=False) 
+    per_size_line("", data, "instructions_per_cycle", extent="ci", color="test", save=False) 
     for data in control_flow_scenarios
-]).save(fig_dir.joinpath("ControlFlow-inst_per_cycle-ci.png"), scale_factor=4)
+]).save(fig_dir.joinpath("ControlFlow-instructions_per_cycle-ci.png"), scale_factor=4)
 
 alt.layer(*cache_lines, *[
     per_size_line("", data, "cache_miss_rate", extent="ci", color="test", save=False) 
@@ -150,14 +174,37 @@ alt.layer(*[
 ]).save(fig_dir.joinpath(f"ControlFlow-branch_miss_rate-ci.png"), scale_factor=4)
 
 
+# layout_scenarios = df.filter(part="Layout").partition_by("scenario")
 
-scenarios = df.partition_by("scenario", "part", include_key=False, as_dict=True)
-for (s, part), data in scenarios.items():
+# alt.layer(*[
+#     per_size_line("", data, "wall_clock", scale="log", extent="ci", color="test", save=False) 
+#     for data in layout_scenarios
+# ]).save(fig_dir.joinpath("Layout-wall_clock-ci.png"), scale_factor=4)
+
+# alt.layer(*[
+#     per_size_line("", data, "instructions_per_cycle", extent="ci", color="test", save=False) 
+#     for data in layout_scenarios
+# ]).save(fig_dir.joinpath("Layout-instructions_per_cycle-ci.png"), scale_factor=4)
+
+# alt.layer(*cache_lines, *[
+#     per_size_line("", data, "cache_miss_rate", extent="ci", color="test", save=False) 
+#     for data in layout_scenarios
+# ]).save(fig_dir.joinpath(f"Layout-cache_miss_rate-ci.png"), scale_factor=4)
+
+# alt.layer(*[
+#     per_size_line("", data, "branch_miss_rate", y_format="%", extent="ci", color="test", save=False) 
+#     for data in layout_scenarios
+# ]).save(fig_dir.joinpath(f"Layout-branch_miss_rate-ci.png"), scale_factor=4)
+
+
+
+scenarios = df.partition_by("scenario", include_key=False, as_dict=True)
+for (s,), data in scenarios.items():
     print(s)
 
     for (col, opts) in [
         ("wall_clock", {"scale": "log"}), 
-        ("inst_per_cycle", {}), 
+        ("instructions_per_cycle", {}), 
         ("cache_miss_rate", {"y_format": "%", "chart_layers": cache_lines}), 
         ("branch_miss_rate", {"y_format": "%"})
         ]:
