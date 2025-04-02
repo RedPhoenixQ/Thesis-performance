531f51684724fb4cdc833261c3ef322d1b32f799
RedPhoenixQ <steve.dark.03@gmail.com>
2025-04-02 20:55:03 +0200

    Refactor chart generation

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from typing import Optional
 import altair as alt
 import polars as pl
 import glob
@@ -41,13 +42,13 @@ df = df.with_columns(
     (pl.col("instructions") / pl.col("cpu_cycles"))
         .alias("inst_per_cycle"),
     pl.when(pl.col("cache_references") > 0)
-        .then(1 - (pl.col("cache_misses") / pl.col("cache_references")))
-        .otherwise(1)
-        .alias("cache_hit_rate"),
+        .then((pl.col("cache_misses") / pl.col("cache_references")))
+        .otherwise(0)
+        .alias("cache_miss_rate"),
     pl.when(pl.col("branch_instructions") > 0)
-        .then(1 - (pl.col("branch_misses") / pl.col("branch_instructions")))
-        .otherwise(1)
-        .alias("branch_hit_rate"),
+        .then((pl.col("branch_misses") / pl.col("branch_instructions")))
+        .otherwise(0)
+        .alias("branch_miss_rate"),
 )
 
 summary = df.group_by("size", "part", "scenario", "kind").agg(
@@ -55,102 +56,73 @@ summary = df.group_by("size", "part", "scenario", "kind").agg(
     pl.std("wall_clock").name.suffix("_std"),
     pl.mean("inst_per_cycle"),
     pl.std("inst_per_cycle").name.suffix("_std"),
-    pl.mean("cache_hit_rate"),
-    pl.std("cache_hit_rate").name.suffix("_std"),
-    pl.mean("branch_hit_rate"),
-    pl.std("branch_hit_rate").name.suffix("_std"),
+    pl.mean("cache_miss_rate"),
+    pl.std("cache_miss_rate").name.suffix("_std"),
+    pl.mean("branch_miss_rate"),
+    pl.std("branch_miss_rate").name.suffix("_std"),
 ).sort("size", "part", "scenario", "kind")
 
 summary.write_csv(fig_dir.joinpath("summary.csv"))
 
-sizes = df.get_column("size").unique()
-columns = list(filter(lambda c: c not in ["part", "scenario", "test", "kind", "size"], df.columns))
+def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined):
+    error_bars = alt.Chart(source).mark_errorbar(extent=extent, ticks=True).encode(
+        alt.X("size").type("ordinal").title("Size"),
+        alt.Y(col,
+            aggregate=agg,
+            scale=alt.Scale(type=scale), 
+            axis=alt.Axis(format=y_format),
+            title=col.replace("_", " ").capitalize(),
+        ),
+        xOffset="kind",
+    )
+    line = alt.Chart(source).mark_bar().encode(
+        alt.X("size").type("ordinal").title("Size"),
+        alt.Y(col, 
+            aggregate=agg,
+            scale=alt.Scale(type=scale), 
+            axis=alt.Axis(format=y_format),
+            title=col.replace("_", " ").capitalize(),
+        ),
+        xOffset="kind",
+        color="kind",
+    )
+    (line + error_bars).save(fig_dir.joinpath(f"{name}-{col}-bar-{extent}.png"), scale_factor=4)
 
-layout = df.filter(part="Layout")
-controlflow = df.filter(part="ControlFlow")
-
-lscenarios = layout.get_column("scenario").unique()
-lkind = layout.get_column("kind").unique()
-
-for s in lscenarios:
-    source = layout.filter(pl.col("scenario") == s)
-
-    print(source)
-
-    error_bars = alt.Chart(source).mark_errorband(extent="ci").encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cache_hit_rate").aggregate("mean"),
+def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined):
+    error_bars = alt.Chart(source).mark_errorband(extent=extent).encode(
+        alt.X("size").type("ordinal").title("Size"),
+        alt.Y(col,
+            aggregate=agg,
+            scale=alt.Scale(type=scale), 
+            axis=alt.Axis(format=y_format),
+            title=col.replace("_", " ").capitalize(),
+        ),
         color="kind",
     )
     line = alt.Chart(source).mark_line().encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cache_hit_rate").aggregate("mean"),
+        alt.X("size").type("ordinal").title("Size"),
+        alt.Y(col,
+            aggregate=agg,
+            scale=alt.Scale(type=scale), 
+            axis=alt.Axis(format=y_format),
+            title=col.replace("_", " ").capitalize(),
+        ),
         color="kind",
     )
-    (error_bars + line).save(fig_dir.joinpath(s + "-cache_hit_rate.png"), scale_factor=4)
-    
-    
-    error_bars = alt.Chart(source).mark_errorbar(extent="stdev", ticks=True).encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cache_hit_rate").aggregate("average"),
-        xOffset="kind",
-    )
-    line = alt.Chart(source).mark_bar().encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cache_hit_rate").aggregate("average"),
-        xOffset="kind",
-        color="kind",
-    )
-    (line + error_bars).save(fig_dir.joinpath(s + "-cache_hit_rate-bar-stdev.png"), scale_factor=4)
-    
-    error_bars = alt.Chart(source).mark_errorbar(extent="ci", ticks=True).encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cache_hit_rate").aggregate("mean"),
-        xOffset="kind",
-    )
-    line = alt.Chart(source).mark_bar().encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cache_hit_rate").aggregate("mean"),
-        xOffset="kind",
-        color="kind",
-    )
-    (line + error_bars).save(fig_dir.joinpath(s + "-cache_hit_rate-bar-ci.png"), scale_factor=4)
+    (line + error_bars).save(fig_dir.joinpath(f"{name}-{col}-line-{extent}.png"), scale_factor=4)
 
+scenarios = df.partition_by("scenario", include_key=False, as_dict=True)
 
-    error_bars = alt.Chart(source).mark_errorband(extent="stdev").encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cpu_cycles").aggregate("mean").scale(type="log"),
-        color="kind",
-    )
-    line = alt.Chart(source).mark_line().encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cpu_cycles").aggregate("mean").scale(type="log"),
-        color="kind",
-    )
-    (line + error_bars).save(fig_dir.joinpath(s + "-cpu_cycles-line-stdev.png"), scale_factor=4)
-    
-    error_bars = alt.Chart(source).mark_errorbar(extent="stdev", ticks=True).encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cpu_cycles").aggregate("average").scale(type="log"),
-        xOffset="kind",
-    )
-    line = alt.Chart(source).mark_bar().encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cpu_cycles").aggregate("average").scale(type="log"),
-        xOffset="kind",
-        color="kind",
-    )
-    (line + error_bars).save(fig_dir.joinpath(s + "-cpu_cycles-bar-stdev.png"), scale_factor=4)
+for (s,), data in scenarios.items():
+    print(s, data)
 
-    error_bars = alt.Chart(source).mark_errorbar(extent="ci", ticks=True).encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cpu_cycles").aggregate("mean").scale(type="log"),
-        xOffset="kind",
-    )
-    line = alt.Chart(source).mark_bar().encode(
-        alt.X("size").type("ordinal"),
-        alt.Y("cpu_cycles").aggregate("mean").scale(type="log"),
-        xOffset="kind",
-        color="kind",
-    )
-    (line + error_bars).save(fig_dir.joinpath(s + "-cpu_cycles-bar-ci.png"), scale_factor=4)
+    for (col, opts) in [
+        ("wall_clock", {"scale": "log"}), 
+        ("inst_per_cycle", {}), 
+        ("cache_miss_rate", {"y_format": "%"}), 
+        ("branch_miss_rate", {"y_format": "%"})
+        ]:
+        per_size_bar(s, data, col, **opts)
+        per_size_bar(s, data, col, **opts, extent="ci")
+        per_size_line(s, data, col, **opts)
+        per_size_line(s, data, col, **opts, extent="ci")
