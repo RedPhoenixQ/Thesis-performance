6daced011c14e376241c735a6ffec7e2a1d27257
RedPhoenixQ <steve.dark.03@gmail.com>
2025-05-12 22:10:37 +0200

    Change "Size" to "Number of Items" to be more descriptive

--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ import glob
 
 def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined, chart_layers=None):
     error_bars = alt.Chart(source).mark_errorbar(extent=extent, ticks=True).encode(
-        alt.X("size", type="ordinal", title="Size"),
+        alt.X("number_of_items", type="ordinal", title="Number of Item"),
         alt.Y(col,
             aggregate=agg,
             scale=alt.Scale(type=scale, base=2), 
@@ -18,7 +18,7 @@ def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="
         xOffset="kind",
     )
     line = alt.Chart(source).mark_bar().encode(
-        alt.X("size", type="ordinal", title="Size"),
+        alt.X("number_of_items", type="ordinal", title="Number of Item"),
         alt.Y(col, 
             aggregate=agg,
             scale=alt.Scale(type=scale, base=2), 
@@ -32,7 +32,7 @@ def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="
 
 def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined, y_title=None, color="kind", save=True, chart_layers=[]):
     error_bars = alt.Chart(source).mark_errorband(extent=extent).encode(
-        alt.X("size", type="quantitative", title="Size", scale=alt.Scale(type="log", base=2, paddingOuter=0)),
+        alt.X("number_of_items", type="quantitative", title="Number of Items", scale=alt.Scale(type="log", base=2, paddingOuter=0)),
         alt.Y(col,
             aggregate=agg,
             scale=alt.Scale(type=scale, base=2), 
@@ -42,7 +42,7 @@ def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent=
         color=color,
     )
     line = alt.Chart(source).mark_line().encode(
-        alt.X("size", type="quantitative", title="Size", scale=alt.Scale(type="log", base=2)),
+        alt.X("number_of_items", type="quantitative", title="Number of Items", scale=alt.Scale(type="log", base=2)),
         alt.Y(col,
             aggregate=agg,
             scale=alt.Scale(type=scale, base=2), 
@@ -77,7 +77,7 @@ for name in filenames:
     split = rest.split("-")
     if len(split) == 4:
         split.pop()
-    [test, kind, size] = split
+    [test, kind, num_items] = split
     df = pl.read_csv(data_dir.joinpath( name), skip_rows_after_header=5)
     match test:
         case "DynamicDispatch":
@@ -90,7 +90,7 @@ for name in filenames:
         pl.lit(part).alias("part"),
         pl.lit(test).alias("test"),
         pl.lit(kind).alias("kind"),
-        pl.lit(int(size)).alias("size"),
+        pl.lit(int(num_items)).alias("number_of_items"),
     )
     df = df.with_columns((pl.col("test") + "-" + pl.col("kind")).alias("Scenario"))
     frames.append(df)
@@ -107,10 +107,10 @@ df = df.with_columns(
         .then((pl.col("branch_misses") / pl.col("branch_instructions")))
         .otherwise(0)
         .alias("branch_miss_rate"),
-).sort("size", "part", "test", "kind")
+).sort("number_of_items", "part", "test", "kind")
 
 # Summary stats
-df.group_by("size", "part", "test", "kind", maintain_order=True).agg(
+df.group_by("number_of_items", "part", "test", "kind", maintain_order=True).agg(
     pl.mean("cpu_cycles"),
     pl.std("cpu_cycles").name.suffix("_std"),
     pl.mean("execution_time"),
@@ -124,7 +124,7 @@ df.group_by("size", "part", "test", "kind", maintain_order=True).agg(
 ).write_csv(fig_dir.joinpath("summary.csv"))
 
 # Instructions per item stats
-(df.with_columns(instructions_per_item=pl.col("instructions") / pl.col("size"))
+(df.with_columns(instructions_per_item=pl.col("instructions") / pl.col("number_of_items"))
     .group_by("part", "Scenario", maintain_order=True)
     .agg(pl.mean("instructions_per_item"), pl.std("instructions_per_item").name.suffix("_std"))
 ).write_csv(fig_dir.joinpath("instructions.csv"))
@@ -132,23 +132,23 @@ df.group_by("size", "part", "test", "kind", maintain_order=True).agg(
 layout_tests = df.filter(part="Layout")
 control_flow_tests = df.filter(part="ControlFlow")
 
-times = control_flow_tests.select("Scenario", "size", "execution_time").partition_by("size", as_dict=True, include_key=False, maintain_order=True)
+times = control_flow_tests.select("Scenario", "number_of_items", "execution_time").partition_by("number_of_items", as_dict=True, include_key=False, maintain_order=True)
 anova = {}
-for (size, ), parts in times.items():
+for (num_items, ), parts in times.items():
     res = stats.f_oneway(*(data.get_column("execution_time") for data in parts.partition_by("Scenario")))
-    anova[str(size)] = res.pvalue
+    anova[str(num_items)] = res.pvalue
 pl.from_dict(anova).write_csv(fig_dir.joinpath("ControlFlow-execution_time-anova.csv"))
 
 tukey = []
-for (size, ), parts in times.items():
+for (num_items, ), parts in times.items():
     scenarios = parts.partition_by("Scenario", as_dict=True, maintain_order=True)
     keys = [key for (key,) in scenarios.keys()]
     res = stats.tukey_hsd(*(data.get_column("execution_time") for data in scenarios.values()))
-    tukey.append({ "size": str(size), } | {
+    tukey.append({ "number_of_items": str(num_items), } | {
         f"{keys[i]} - {keys[j]}": res.pvalue[i, j]
         for (i,j) in itertools.combinations(range(len(keys)), 2)
     })
-pl.from_dicts(tukey).transpose(include_header=True, header_name="Combinations", column_names="size").write_csv(fig_dir.joinpath("ControlFlow-execution_time-tukey.csv"))
+pl.from_dicts(tukey).transpose(include_header=True, header_name="Combinations", column_names="number_of_items").write_csv(fig_dir.joinpath("ControlFlow-execution_time-tukey.csv"))
 
 # Indicator line for cache sizes
 cache_lines = [
@@ -162,26 +162,26 @@ cache_lines.extend([
     for i, line in enumerate(cache_lines, 1)
 ])
 
-layout_kinds = layout_tests.select("test", "kind", "execution_time", "size").partition_by("kind", as_dict=True, maintain_order=True)
+layout_kinds = layout_tests.select("test", "kind", "execution_time", "number_of_items").partition_by("kind", as_dict=True, maintain_order=True)
 layout_diff = layout_kinds.get(("AoS",)).rename({"execution_time": "AoS"}).hstack([layout_kinds.get(("SoA",)).get_column("execution_time").alias("SoA")])
-diff = (layout_diff.group_by("test", "size").agg(diff=pl.mean("SoA") / pl.mean("AoS") - pl.lit(1)))
+diff = (layout_diff.group_by("test", "number_of_items").agg(diff=pl.mean("SoA") / pl.mean("AoS") - pl.lit(1)))
 per_size_line("Layout-execution_time", diff, "diff", color="test", y_format="%", y_title="SoA execution time compared to AoS", chart_layers=cache_lines)
 
-diff.select("test", "size", "diff").write_csv(fig_dir.joinpath("Layout-execution_time-diff.csv"))
+diff.select("test", "number_of_items", "diff").write_csv(fig_dir.joinpath("Layout-execution_time-diff.csv"))
 
 # T-test AoS vs SoA
-layout_diff.sort("test", "size").group_by("test", "size", maintain_order=True).map_groups(
+layout_diff.sort("test", "number_of_items").group_by("test", "number_of_items", maintain_order=True).map_groups(
     lambda group: pl.from_dict({
         "test": group.get_column("test").first(),
-        "size": group.get_column("size").first(),
+        "number_of_items": group.get_column("number_of_items").first(),
         "pvalue": stats.ttest_ind(group.get_column("SoA"), group.get_column("AoS"), equal_var=False).pvalue
     })
-).pivot("size", index="test", values="pvalue").write_csv(fig_dir.joinpath("Layout-execution_time-ttest-pvalue.csv"))
+).pivot("number_of_items", index="test", values="pvalue").write_csv(fig_dir.joinpath("Layout-execution_time-ttest-pvalue.csv"))
 
 diff = control_flow_tests.join(
-    control_flow_tests.filter(Scenario="DD-unsorted").group_by("size").agg(DD_mean=pl.mean("execution_time")), 
-    on="size"
-).group_by("Scenario", "size").agg(diff=pl.mean("execution_time") / pl.first("DD_mean") - pl.lit(1))
+    control_flow_tests.filter(Scenario="DD-unsorted").group_by("number_of_items").agg(DD_mean=pl.mean("execution_time")), 
+    on="number_of_items"
+).group_by("Scenario", "number_of_items").agg(diff=pl.mean("execution_time") / pl.first("DD_mean") - pl.lit(1))
 per_size_line("ControlFlow-execution_time", diff, "diff", color="Scenario", y_format="%", chart_layers=cache_lines)
 
 for extent in ["ci", "stdev"]:
