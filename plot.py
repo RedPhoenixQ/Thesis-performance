a287f6b3a015bfd1f8cb97bbbc664d0dc9ebebd7
RedPhoenixQ <steve.dark.03@gmail.com>
2025-05-19 18:49:08 +0200

    Dash lines based on test kind for better readability

--- plot.py
+++ plot.py
@@ -15,7 +15,7 @@ def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="
             axis=alt.Axis(format=y_format),
             title=col.replace("_", " ").capitalize(),
         ),
-        xOffset="kind",
+        xOffset="Kind",
     )
     line = alt.Chart(source).mark_bar().encode(
         alt.X("number_of_items", type="ordinal", title="Number of Item"),
@@ -25,12 +25,12 @@ def per_size_bar(name: str, source: pl.DataFrame, col: str, agg="mean", extent="
             axis=alt.Axis(format=y_format),
             title=col.replace("_", " ").capitalize(),
         ),
-        xOffset="kind",
-        color="kind",
+        xOffset="Kind",
+        color="Kind",
     )
     (line + error_bars).save(fig_dir.joinpath(f"{name}-{col}-bar-{extent}.png"), scale_factor=4)
 
-def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined, y_title=None, color="kind", save=True, chart_layers=[]):
+def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent="stdev", scale=alt.Undefined, y_format=alt.Undefined, y_title=None, color="Kind", dash=alt.Undefined, save=True, chart_layers=[]):
     error_bars = alt.Chart(source).mark_errorband(extent=extent).encode(
         alt.X("number_of_items", type="quantitative", title="Number of Items", scale=alt.Scale(type="log", base=2, paddingOuter=0)),
         alt.Y(col,
@@ -49,6 +49,7 @@ def per_size_line(name: str, source: pl.DataFrame, col: str, agg="mean", extent=
             axis=alt.Axis(format=y_format), 
             title=col.replace("_", " ").capitalize() if y_title is None else y_title,
         ),
+        alt.StrokeDash(field=dash),
         color=color,
     )
     chart = alt.layer(*chart_layers, line, error_bars)
@@ -87,12 +88,12 @@ for name in filenames:
         case "ExistentialProcessing":
             test = "EP"
     df = df.with_columns(
-        pl.lit(part).alias("part"),
-        pl.lit(test).alias("test"),
-        pl.lit(kind).alias("kind"),
+        pl.lit(part).alias("Part"),
+        pl.lit(test).alias("Test"),
+        pl.lit(kind).alias("Kind"),
         pl.lit(int(num_items)).alias("number_of_items"),
     )
-    df = df.with_columns((pl.col("test") + "-" + pl.col("kind")).alias("Scenario"))
+    df = df.with_columns((pl.col("Test") + "-" + pl.col("Kind")).alias("Scenario"))
     frames.append(df)
 
 df: pl.DataFrame = pl.concat(frames)
@@ -107,10 +108,10 @@ df = df.with_columns(
         .then((pl.col("branch_misses") / pl.col("branch_instructions")))
         .otherwise(0)
         .alias("branch_miss_rate"),
-).sort("number_of_items", "part", "test", "kind")
+).sort("number_of_items", "Part", "Test", "Kind")
 
 # Summary stats
-df.group_by("number_of_items", "part", "test", "kind", maintain_order=True).agg(
+df.group_by("number_of_items", "Part", "Test", "Kind", maintain_order=True).agg(
     pl.mean("cpu_cycles"),
     pl.std("cpu_cycles").name.suffix("_std"),
     pl.mean("execution_time"),
@@ -125,12 +126,12 @@ df.group_by("number_of_items", "part", "test", "kind", maintain_order=True).agg(
 
 # Instructions per item stats
 (df.with_columns(instructions_per_item=pl.col("instructions") / pl.col("number_of_items"))
-    .group_by("part", "Scenario", maintain_order=True)
+    .group_by("Part", "Scenario", maintain_order=True)
     .agg(pl.mean("instructions_per_item"), pl.std("instructions_per_item").name.suffix("_std"))
 ).write_csv(fig_dir.joinpath("instructions.csv"))
 
-layout_tests = df.filter(part="Layout")
-control_flow_tests = df.filter(part="ControlFlow")
+layout_tests = df.filter(Part="Layout")
+control_flow_tests = df.filter(Part="ControlFlow")
 
 correlation = []
 for (Scenario,), data in df.partition_by("Scenario", as_dict=True, maintain_order=True).items():
@@ -226,36 +227,36 @@ cache_lines.extend([
     for i, line in enumerate(cache_lines, 1)
 ])
 
-layout_kinds = layout_tests.select("test", "kind", "execution_time", "number_of_items").partition_by("kind", as_dict=True, maintain_order=True)
+layout_kinds = layout_tests.select("Test", "Kind", "execution_time", "number_of_items").partition_by("Kind", as_dict=True, maintain_order=True)
 layout_diff = layout_kinds.get(("AoS",)).rename({"execution_time": "AoS"}).hstack([layout_kinds.get(("SoA",)).get_column("execution_time").alias("SoA")])
-diff = (layout_diff.group_by("test", "number_of_items").agg(diff=pl.lit(1) - pl.mean("SoA") / pl.mean("AoS")))
-per_size_line("Layout-execution_time", diff, "diff", color="test", y_format="%", y_title="SoA execution time compared to AoS", chart_layers=cache_lines)
+diff = (layout_diff.group_by("Test", "number_of_items").agg(diff=pl.lit(1) - pl.mean("SoA") / pl.mean("AoS")))
+per_size_line("Layout-execution_time", diff, "diff", color="Test", y_format="%", y_title="SoA execution time compared to AoS", chart_layers=cache_lines)
 
-diff.select("test", "number_of_items", "diff").write_csv(fig_dir.joinpath("Layout-execution_time-diff.csv"))
+diff.select("Test", "number_of_items", "diff").write_csv(fig_dir.joinpath("Layout-execution_time-diff.csv"))
 
 # T-test AoS vs SoA
-layout_diff.sort("test", "number_of_items").group_by("test", "number_of_items", maintain_order=True).map_groups(
+layout_diff.sort("Test", "number_of_items").group_by("Test", "number_of_items", maintain_order=True).map_groups(
     lambda group: pl.from_dict({
-        "test": group.get_column("test").first(),
+        "Test": group.get_column("Test").first(),
         "number_of_items": group.get_column("number_of_items").first(),
         "pvalue": stats.ttest_ind(group.get_column("SoA"), group.get_column("AoS"), equal_var=False).pvalue
     })
-).pivot("number_of_items", index="test", values="pvalue").write_csv(fig_dir.joinpath("Layout-execution_time-ttest-pvalue.csv"))
+).pivot("number_of_items", index="Test", values="pvalue").write_csv(fig_dir.joinpath("Layout-execution_time-ttest-pvalue.csv"))
 
 diff = control_flow_tests.join(
     control_flow_tests.filter(Scenario="DD-unsorted").group_by("number_of_items").agg(DD_mean=pl.mean("execution_time")), 
     on="number_of_items"
-).group_by("Scenario", "number_of_items").agg(diff=pl.lit(1) - pl.mean("execution_time") / pl.first("DD_mean"))
-per_size_line("ControlFlow-execution_time", diff, "diff", color="Scenario", y_format="%", chart_layers=cache_lines)
+).group_by("Scenario", "Kind", "number_of_items").agg(diff=pl.lit(1) - pl.mean("execution_time") / pl.first("DD_mean"))
+per_size_line("ControlFlow-execution_time", diff, "diff", color="Scenario", dash="Kind", y_format="%", chart_layers=cache_lines)
 
 for extent in ["ci", "stdev"]:
     # Layout
-    per_size_line("Layout", layout_tests, "execution_time", scale="log", extent=extent, color="Scenario", chart_layers=cache_lines) 
-    per_size_line("Layout", layout_tests, "instructions_per_cycle", extent=extent, color="Scenario", chart_layers=cache_lines) 
-    per_size_line("Layout", layout_tests, "cache_miss_rate", y_format="%", extent=extent, color="Scenario", chart_layers=cache_lines) 
+    per_size_line("Layout", layout_tests, "execution_time", scale="log", extent=extent, color="Scenario", dash="Kind", chart_layers=cache_lines) 
+    per_size_line("Layout", layout_tests, "instructions_per_cycle", extent=extent, color="Scenario", dash="Kind", chart_layers=cache_lines) 
+    per_size_line("Layout", layout_tests, "cache_miss_rate", y_format="%", extent=extent, color="Scenario", dash="Kind", chart_layers=cache_lines) 
 
     # Control flow
-    per_size_line("ControlFlow", control_flow_tests, "execution_time", scale="log", extent=extent, color="Scenario", chart_layers=cache_lines)
-    per_size_line("ControlFlow", control_flow_tests, "instructions_per_cycle", extent=extent, color="Scenario", chart_layers=cache_lines) 
-    per_size_line("ControlFlow", control_flow_tests, "cache_miss_rate", y_format="%", extent=extent, color="Scenario", chart_layers=cache_lines) 
-    per_size_line("ControlFlow", control_flow_tests, "branch_miss_rate", y_format="%", extent=extent, color="Scenario", chart_layers=cache_lines) 
+    per_size_line("ControlFlow", control_flow_tests, "execution_time", scale="log", extent=extent, color="Scenario", dash="Kind", chart_layers=cache_lines)
+    per_size_line("ControlFlow", control_flow_tests, "instructions_per_cycle", extent=extent, color="Scenario", dash="Kind", chart_layers=cache_lines) 
+    per_size_line("ControlFlow", control_flow_tests, "cache_miss_rate", y_format="%", extent=extent, color="Scenario", dash="Kind", chart_layers=cache_lines) 
+    per_size_line("ControlFlow", control_flow_tests, "branch_miss_rate", y_format="%", extent=extent, color="Scenario", dash="Kind", chart_layers=cache_lines) 
